88a87dbdb0ce4b8771ee228b8785d4c397b9ec36
99lm99 <109356346+99lm99@users.noreply.github.com>
2022-08-15 17:15:49 +0300

    Add task1

---
+++ Sem2/Task1.cs
@@ -0,0 +1,18 @@
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int ReadInt(string message)
+{
+    Console.Write(message);   
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number = ReadInt("Введите трехзначное число: ");
+Console.WriteLine(DeleteSecondDigit(number));
+int DeleteSecondDigit(int number)
+{
+    int first = number / 10;
+    int second = first % 10;
+    return second;
+}
